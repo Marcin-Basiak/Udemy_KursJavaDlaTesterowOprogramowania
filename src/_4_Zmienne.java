a6574f391c346e5dc1e326519c2baf74f6038cd6
Marcin <marcinbasiak1990@gmail.com>
2025-07-09 13:52:22 +0200

    Add class Zmienne

---
+++ src/_4_Zmienne.java
@@ -0,0 +1,32 @@
+// Zarezerwowane miejsce w pamieci z okreslonym typem danych
+public class _4_Zmienne {
+
+    public static void main(String[] args) {
+        // Deklaracja, to podanie typu oraz nazwy zmiennej
+        String city;
+        String firstName, secondName, lastName;
+
+        // Inicjalizacja, to podane typu, nazwy oraz wartosci zmiennej
+        String county = "Poland";
+
+        // Sama deklaracja
+        int age, height, weight;
+
+        // Inicjalizacja wczesniej zadeklarowanych zmiennych
+        age = 25;
+        height = 170;
+        weight = 60;
+
+
+        String name = "Adam";
+        int firstNumber = 1;
+        int secondNumber = 2;
+        int result = firstNumber + secondNumber;
+
+        System.out.println(name);
+        name = "Kasia";
+        System.out.println(name);
+
+        System.out.println(result);
+    }
+}
