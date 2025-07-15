e912f7c39ef5bd885e8a85eb19d23c69ecb96b4f
Marcin <marcinbasiak1990@gmail.com>
2025-07-15 17:55:15 +0200

    Add class MetodaStatycznaMainTest

---
+++ src/_40_MetodaStatycznaMainTest.java
@@ -0,0 +1,9 @@
+// Metoda "main" jest metoda statyczna
+
+public class _40_MetodaStatycznaMainTest {
+
+    public static void main(String[] args) {
+        String[] argumenty = new String[0];
+        _39_MetodyStatyczneTest.main(argumenty);
+    }
+}
