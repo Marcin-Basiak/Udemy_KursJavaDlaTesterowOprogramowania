7a56c980ffa6ad678bc85943735e9361de131427
Marcin <marcinbasiak1990@gmail.com>
2025-07-17 14:22:13 +0200

    Add class AplikacjaIOsZadanie

---
+++ src/_51_AplikacjaIOsZadanie.java
@@ -0,0 +1,10 @@
+public class _51_AplikacjaIOsZadanie extends _49_AplikacjaZadanie {
+
+    public _51_AplikacjaIOsZadanie(String nazwaAplikacji, String wersjaAplikacji){
+        super(nazwaAplikacji, wersjaAplikacji);
+    }
+
+    public void uruchomAplikacjeIOs(){
+        System.out.println("Uruchamiam aplikacje dla systemu iOS:");
+    }
+}
