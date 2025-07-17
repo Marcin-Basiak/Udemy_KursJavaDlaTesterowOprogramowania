7e7d9b61b3915d97d2a757ce3921ce10f4ffd04b
Marcin <marcinbasiak1990@gmail.com>
2025-07-17 14:20:14 +0200

    Add class OsobaDziedziczenie

---
+++ src/_41_OsobaDziedziczenie.java
@@ -0,0 +1,15 @@
+// Klasa bazowa/nadrzedna, z ktorej inne klasy potomne/podrzedne (_42_Pilkarz, _43_Nauczyciel) beda dziedziczyly pola i metody
+
+public class _41_OsobaDziedziczenie {
+
+    public String imie;
+    public int wiek;
+
+    public void jedz(){
+        System.out.println("Lubie rosol.");
+    }
+
+    public void idz(){
+        System.out.println("Lubie chodzic.");
+    }
+}
