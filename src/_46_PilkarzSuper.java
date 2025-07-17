05b65598b299b432d9e7ace2ea8ce733dd27d863
Marcin <marcinbasiak1990@gmail.com>
2025-07-17 14:21:19 +0200

    Add class PilkarzSuper

---
+++ src/_46_PilkarzSuper.java
@@ -0,0 +1,16 @@
+public class _46_PilkarzSuper extends _45_OsobaDziedziczenieSuper {
+
+    public String nazwaKlubu;
+
+    // Konstruktor zawierajacy slowo kluczowe "super", ktore powoduje wywolanie konstruktora z klasy nadrzednej "_45_OsobaDziedziczenieSuper"
+    // Slowo kluczowe "super" czyli wywolanie konstruktora z klasy bazowej/nadrzednej musi znalezc się na pierwszym miejscu w konstruktorze z klasy potomnej/podrzednej
+    public _46_PilkarzSuper(String imie, int wiek, String nazwaKlubu) {
+        super(imie, wiek);
+        this.nazwaKlubu = nazwaKlubu;
+        System.out.println("Wywolanie konstruktora z klasy potomnej/podrzednej \"_46_PilkarzSuper\":");
+    }
+
+    public void grajWPilke(){
+        System.out.println("Lubie grac w pilke w zespole " + nazwaKlubu);
+    }
+}
