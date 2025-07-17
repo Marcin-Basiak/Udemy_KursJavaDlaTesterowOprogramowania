903bbc9de5d43ea7e33f5c841b8c7e2ba5d06909
Marcin <marcinbasiak1990@gmail.com>
2025-07-17 14:22:02 +0200

    Add class AplikacjaAndroidZadanie

---
+++ src/_50_AplikacjaAndroidZadanie.java
@@ -0,0 +1,10 @@
+public class _50_AplikacjaAndroidZadanie extends _49_AplikacjaZadanie {
+
+    public _50_AplikacjaAndroidZadanie(String nazwaAplikacji, String wersjaAplikacji){
+        super(nazwaAplikacji, wersjaAplikacji);
+    }
+
+    public void uruchomAplikacjeAndroid(){
+        System.out.println("Uruchamiam aplikacje dla systemu Android:");
+    }
+}
