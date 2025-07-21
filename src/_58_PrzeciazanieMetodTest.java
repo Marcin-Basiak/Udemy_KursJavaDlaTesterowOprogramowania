6fffed65a4f80af2ef23f166d4f47c90fd547933
Marcin <marcinbasiak1990@gmail.com>
2025-07-21 18:24:15 +0200

    Add class PrzeciazanieMetodTest

---
+++ src/_58_PrzeciazanieMetodTest.java
@@ -0,0 +1,12 @@
+public class _58_PrzeciazanieMetodTest {
+
+    public static void main(String[] args) {
+
+        _57_PrzeciazanieMetod dodawanie = new _57_PrzeciazanieMetod();
+        dodawanie.dodaj();
+        dodawanie.dodaj(2);
+        dodawanie.dodaj(3, 5);
+        dodawanie.dodaj(1, 2, 3);
+        dodawanie.dodaj("Test");
+    }
+}
